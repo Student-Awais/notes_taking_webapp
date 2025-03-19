ac34392dbc542caf4630c2b0ac0cfa338198ce88
Muhammad Awais Abid Hussain <kingyt6624@gmail.com>
2025-03-19 18:06:01 +0500

    Finalized Notes Taking App with EJS

--- index.js
+++ index.js
@@ -2,12 +2,51 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const fs = require("fs");
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.render("index");
+  fs.readdir("./files", (err, files) => {
+    res.render("index", { files: files });
+  });
+});
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    }
+  );
+});
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", { filename: req.params.filename });
+});
+app.post("/edit", (req, res) => {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      if (err) {
+        console.error(err);
+      }
+      res.redirect("/");
+    }
+  );
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
